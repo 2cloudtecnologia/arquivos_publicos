28af21c98f27577ae0c8939a2d0d0a3fdd5cd10e
2cloudtecnologia <70173926+2cloudtecnologia@users.noreply.github.com>
2022-06-15 11:28:03 -0300

    Update osinfo.sh

--- osinfo.sh
+++ osinfo.sh
@@ -2,3 +2,4 @@ cd /etc/zabbix/zabbix_agentd.d
 wget https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/os.conf
 systemctl restart zabbix-agent
 zabbix_agentd -t os.info
+zabbix_agentd -t os.info.CentOS6

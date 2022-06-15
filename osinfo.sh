2af3b2b879e17fee40bc27ca854dd6a49e464466
2cloudtecnologia <70173926+2cloudtecnologia@users.noreply.github.com>
2022-06-15 13:56:00 -0300

    Update osinfo.sh

--- osinfo.sh
+++ osinfo.sh
@@ -3,12 +3,12 @@ DIR="/etc/zabbix/zabbix_agentd.d"
 ctl() {
     if hash hostnamectl 2>/dev/null; then
         cd $DIR
-        wget https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/os.conf -O os.conf
+        wget --no-check-certificate https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/os.conf -O os.conf
         systemctl restart zabbix-agent
         zabbix_agentd -t os.info
     else
         cd $DIR
-        wget https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/os_centos6.conf -O os.conf
+        wget --no-check-certificate https://raw.githubusercontent.com/2cloudtecnologia/arquivos_publicos/main/os_centos6.conf -O os.conf
         service zabbix-agent restart
         zabbix_agentd -t os.info
     fi
